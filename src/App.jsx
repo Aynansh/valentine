01c171ed3e897c557f0e3c5f0e05e3325bdcb996
Aynansh <aynansh2002@gmail.com>
2026-01-18 12:08:29 +0530

    Update font styling and UI tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -42,30 +42,22 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="text-4xl md:text-6xl font-bold my-4">
+          <div className="font-poppins text-4xl md:text-6xl font-bold my-4">
             Ok Yayyyyy!!!
           </div>
         </>
       ) : (
         <>
           <img
-            src={lovesvg}
-            className="fixed animate-pulse top-10 md:left-24 left-6 md:w-40 w-28"
-          />
-          <img
-            src={lovesvg2}
-            className="fixed bottom-16 -z-10 animate-pulse md:right-24 right-10 md:w-40 w-32"
-          />
-          <img
             className="h-[230px] rounded-lg shadow-lg"
-            src="https://gifdb.com/images/high/cute-Love-bear-roses-ou7zho5oosxnpo6k.gif"
+            src="https://media.tenor.com/OrxXcqX25KcAAAAi/dudu-bubu-love-gif.gif"
           />
-          <h1 className="text-4xl md:text-6xl my-4 text-center">
-            Will you be my Valentine?
+          <h1 className="font-poppins font-semibold text-4xl md:text-6xl my-4 text-center">
+            Ittu sa bebu ji, will you be my Valentine?
           </h1>
           <div className="flex flex-wrap justify-center gap-2 items-center">
             <button
-              className={`bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg mr-4`}
+              className={`bg-green-500 hover:bg-green-600 text-white font-poppins font-bold py-2 px-4 rounded-lg mr-4`}
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
@@ -73,29 +65,13 @@ export default function Page() {
             </button>
             <button
               onClick={handleNoClick}
-              className=" bg-rose-500 hover:bg-rose-600 rounded-lg text-white font-bold py-2 px-4"
+              className=" bg-rose-500 hover:bg-rose-600 font-poppins rounded-lg text-white font-bold py-2 px-4"
             >
               {noCount === 0 ? "No" : getNoButtonText()}
             </button>
           </div>
         </>
       )}
-      <Footer />
     </div>
   );
 }
-
-const Footer = () => {
-  return (
-    <a
-      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/Xeven777/valentine"
-      target="__blank"
-    >
-      Made with{" "}
-      <span role="img" aria-label="heart">
-        ❤️
-      </span>
-    </a>
-  );
-};
